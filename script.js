8c98b5a48b69adfbf88fe09a02dec746010ec946
Dennis Ammen <dennis.ammen@outlook.com>
2022-09-29 22:30:48 +0200

    Check for Winner Functionality

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function setPieceId(fieldEl) {
 function playerTurn(fieldIndex) {
     if (gameBoard[fieldIndex]) return;
     setGamePiece(fieldIndex);
+    checkWinner();
     changePlayer();
     drawGameBoard();
 }
@@ -48,6 +49,45 @@ function drawGameBoard() {
             fieldEl.children[1].classList.remove('d-none');
         }
     })
+}
+
+function checkWinner() {
+    // Check Horizontal
+    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] != 0) {
+        console.log('winner');
+    };
+    if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[3] != 0) {
+        console.log('winner');
+    };
+    if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]  && gameBoard[6] != 0) {
+        console.log('winner');
+    };
+
+    // Check Vertical
+    if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[0] != 0) {
+        console.log('winner');
+    };
+    if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[1] != 0) {
+        console.log('winner');
+    };
+    if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[2] != 0) {
+        console.log('winner');
+    };
+
+    // Check Diagonal
+    if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[0] != 0) {
+        console.log('winner');
+    };
+    if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[2] != 0) {
+        console.log('winner');
+    };
+
+    // for (let index = 0; index < 3; index++) {
+    //     // Check Vertical
+    //     if (gameBoard[index] === gameBoard[index + 3] && gameBoard[index + 3] === gameBoard[ index + 6] && gameBoard[index] != 0) {
+    //         console.log('winner', index);
+    //     };
+    // }
 }
 
 
