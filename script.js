adc3dd3157a5ef75223bc8ba3ed5098b7c3c6c7c
Dennis Ammen <dennis.ammen@outlook.com>
2022-09-30 00:22:05 +0200

    Check for Draw

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const fieldsEl = document.querySelectorAll('td');
+const FIELDS_EL = document.querySelectorAll('td');
 
 let gameBoard = new Array(9);
 let currentPlayer = 1;
+let isDraw = false;
+let isGameOver = false;
 
 
 function init() {
@@ -13,7 +15,7 @@ function init() {
 }
 
 function setupFields() {
-    fieldsEl.forEach((fieldEl, fieldIndex) => {
+    FIELDS_EL.forEach((fieldEl, fieldIndex) => {
         fieldEl.addEventListener('click', () => playerTurn(fieldIndex));
         setPieceId(fieldEl);
     });
@@ -43,20 +45,20 @@ function changePlayer() {
 }
 
 function changePlayerLabel() {
-    const playerOneLabel = document.getElementById('player1-label');
-    const playerTwoLabel = document.getElementById('player2-label');
+    const PLAYER_ONE_LABEL = document.getElementById('player1-label');
+    const PLAYER_TWO_LABEL = document.getElementById('player2-label');
 
     if (currentPlayer === 1) {
-        playerOneLabel.classList.remove('d-none');
-        playerTwoLabel.classList.add('d-none');
+        PLAYER_ONE_LABEL.classList.remove('d-none');
+        PLAYER_TWO_LABEL.classList.add('d-none');
     } else {
-        playerOneLabel.classList.add('d-none');
-        playerTwoLabel.classList.remove('d-none');
+        PLAYER_ONE_LABEL.classList.add('d-none');
+        PLAYER_TWO_LABEL.classList.remove('d-none');
     }
 }
 
 function drawGameBoard() {
-    fieldsEl.forEach((fieldEl, fieldIndex) => {
+    FIELDS_EL.forEach((fieldEl, fieldIndex) => {
         if (gameBoard[fieldIndex] === 1) {
             fieldEl.children[0].classList.remove('d-none');
         } else if (gameBoard[fieldIndex] === 2) {
@@ -68,36 +70,51 @@ function drawGameBoard() {
 function checkWinner() {
     // Check Horizontal
     if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
     if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[3] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
     if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]  && gameBoard[6] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
 
     // Check Vertical
     if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[0] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
     if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[1] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
     if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[2] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
 
     // Check Diagonal
     if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[0] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
     if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[2] != 0) {
+        isGameOver = true;
         console.log('winner', currentPlayer);
     };
 
     // Check Draw
+    isDraw = true;
+    for (let field of gameBoard) {
+        if (field === 0) {
+            isDraw = false
+        };
+    }
 
+    if (isDraw && !isGameOver) console.log('draw');
 
     // for (let index = 0; index < 3; index++) {
     //     // Check Vertical
