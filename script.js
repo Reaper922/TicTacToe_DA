7740bd1e65161cc8089a25f66a64e96f0fc193bf
Dennis Ammen <dennis.ammen@outlook.com>
2022-09-29 23:05:02 +0200

    Display Player Label

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function playerTurn(fieldIndex) {
     setGamePiece(fieldIndex);
     checkWinner();
     changePlayer();
+    changePlayerLabel();
     drawGameBoard();
 }
 
@@ -39,6 +40,19 @@ function setGamePiece(fieldIndex) {
 
 function changePlayer() {
     currentPlayer = currentPlayer === 1 ? 2 : 1;
+}
+
+function changePlayerLabel() {
+    const playerOneLabel = document.getElementById('player1-label');
+    const playerTwoLabel = document.getElementById('player2-label');
+
+    if (currentPlayer === 1) {
+        playerOneLabel.classList.remove('d-none');
+        playerTwoLabel.classList.add('d-none');
+    } else {
+        playerOneLabel.classList.add('d-none');
+        playerTwoLabel.classList.remove('d-none');
+    }
 }
 
 function drawGameBoard() {
@@ -54,33 +68,36 @@ function drawGameBoard() {
 function checkWinner() {
     // Check Horizontal
     if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
     if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[3] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
     if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]  && gameBoard[6] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
 
     // Check Vertical
     if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[0] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
     if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[1] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
     if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[2] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
 
     // Check Diagonal
     if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[0] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
     if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[2] != 0) {
-        console.log('winner');
+        console.log('winner', currentPlayer);
     };
+
+    // Check Draw
+
 
     // for (let index = 0; index < 3; index++) {
     //     // Check Vertical
